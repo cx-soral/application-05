c2d92e12014cb14b79438b4430f7aeb8f0a17c97
X-I-A <develop@x-i-a.com>
2024-02-19 21:56:13 +0100

    feat: application test

--- iac/modules/gcp-module-dataset/main.tf
+++ iac/modules/gcp-module-dataset/main.tf
@@ -1,12 +1,13 @@
 locals {
   landscape = yamldecode(file(var.landscape_file))
   dataset_index = yamldecode(file(var.index_file))
+  project_prefix = local.landscape["settings"]["project_prefix"]
 }
 
 resource "google_bigquery_dataset" "bigquery_dataset" {
   for_each = local.dataset_index
 
-  project =
+  project = "${local.project_prefix}${var.env_name}"
   dataset_id = each.key
   location   = each.value["region"]
 }
